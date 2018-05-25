92847f1a9badf407cc4e44961b18a76539e9a142
Paul  Karikari <paulkarikari1@gmail.com>
2018-05-25 18:30:05 +0000

    fix migration typo

--- db/migrate/20180525174420_create_products.rb
+++ db/migrate/20180525174420_create_products.rb
@@ -3,7 +3,7 @@ class CreateProducts < ActiveRecord::Migration[5.2]
     create_table :products do |t|
       t.string :title
       t.text :description
-      t.decimal :price, pricision: 8, scale: 2
+      t.decimal :price, precision: 8, scale: 2
       t.string :image_url
 
       t.timestamps
