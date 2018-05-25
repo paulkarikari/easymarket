8b7dbc322b80a38c5516b75377b1a8be257ca4aa
Paul  Karikari <paulkarikari1@gmail.com>
2018-05-25 17:55:23 +0000

    scaffold product model

---
+++ db/migrate/20180525174420_create_products.rb
@@ -0,0 +1,12 @@
+class CreateProducts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :products do |t|
+      t.string :title
+      t.text :description
+      t.decimal :price, pricision: 8, scale: 2
+      t.string :image_url
+
+      t.timestamps
+    end
+  end
+end
