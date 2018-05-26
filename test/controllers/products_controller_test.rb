0d051dd7ac9ee59741da8225d4a3529a760b0d26
Paul  Karikari <paulkarikari1@gmail.com>
2018-05-26 13:25:53 +0000

    add product controller test

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,8 +1,14 @@
-require 'test_helper'
+require "test_helper"
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @product = products(:one)
+    @update = {
+      title: "Lorem Ipsum",
+      description: "Wibbles are fun",
+      image_url: "https://uploads.toptal.io/blog/image/404/toptal-blog-image-1401804250501.png",
+      price: 19.95,
+    }
   end
 
   test "should get index" do
@@ -16,8 +22,8 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should create product" do
-    assert_difference('Product.count') do
-      post products_url, params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
+    assert_difference("Product.count") do
+      post products_url, params: {product: @update}
     end
 
     assert_redirected_to product_url(Product.last)
@@ -34,12 +40,12 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update product" do
-    patch product_url(@product), params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
+    patch product_url(@product), params: {product: @update}
     assert_redirected_to product_url(@product)
   end
 
   test "should destroy product" do
-    assert_difference('Product.count', -1) do
+    assert_difference("Product.count", -1) do
       delete product_url(@product)
     end
 
