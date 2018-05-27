481e97a104e7a2a5a1c5a3240636239f5c138f69
Paul  Karikari <paulkarikari1@gmail.com>
2018-05-27 15:14:55 +0000

    add cart button

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -1,4 +1,4 @@
-require 'test_helper'
+require "test_helper"
 
 class LineItemsControllerTest < ActionDispatch::IntegrationTest
   setup do
@@ -16,11 +16,12 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should create line_item" do
-    assert_difference('LineItem.count') do
-      post line_items_url, params: { line_item: { cart_id: @line_item.cart_id, product_id: @line_item.product_id } }
+    assert_difference("LineItem.count") do
+      post line_items_url, params: {product_id: products(:ruby).id}
     end
-
-    assert_redirected_to line_item_url(LineItem.last)
+    follow_redirect!
+    assert_select "h2", "Your Pragmatic Cart"
+    assert_select "li", "programming ruby"
   end
 
   test "should show line_item" do
@@ -34,12 +35,12 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update line_item" do
-    patch line_item_url(@line_item), params: { line_item: { cart_id: @line_item.cart_id, product_id: @line_item.product_id } }
+    patch line_item_url(@line_item), params: {line_item: {cart_id: @line_item.cart_id, product_id: @line_item.product_id}}
     assert_redirected_to line_item_url(@line_item)
   end
 
   test "should destroy line_item" do
-    assert_difference('LineItem.count', -1) do
+    assert_difference("LineItem.count", -1) do
       delete line_item_url(@line_item)
     end
 
